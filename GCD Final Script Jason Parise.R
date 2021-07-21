950a659dd439a432060f4b2b7c49ddd534103e9f
Jason Parise <68075949+jparise858@users.noreply.github.com>
2021-07-21 13:19:50 -0700

    Add files via upload

---
+++ GCD Final Script Jason Parise.R
@@ -0,0 +1,18 @@
+# Repeat your calls to gather() and separate(), but this time
+# use the %>% operator to chain the commands together without
+# storing an intermediate result.
+#
+# If this is your first time seeing the %>% operator, check
+# out ?chain, which will bring up the relevant documentation.
+# You can also look at the Examples section at the bottom
+# of ?gather and ?separate.
+#
+# The main idea is that the result to the left of %>%
+# takes the place of the first argument of the function to
+# the right. Therefore, you OMIT THE FIRST ARGUMENT to each
+# function.
+#
+all_of(students2) %>%
+  gather(students2, key = sex_class, value = count, -grade) %>%
+  separate(data = res, col = sex_class, into = c("sex", "class")) %>%
+  print
